a3d762a3124475c451117bc637aae55ad4875319
Ricardo Vera <ricardovera71@gmail.com>
2022-12-22 00:16:09 -0400

    fix: fixed some bugs

--- database/database.js
+++ database/database.js
@@ -3,8 +3,8 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "localhost",
   database: "testingSQL",
-  user: "root",
-  password: "",
+  user: "dev",
+  password: "secret",
 });
 
 module.exports = connection;
